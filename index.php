8da5db41588f40ba95c2ce4709f5de2296af6eaf
Sergey Papas <reyes23@mail.ru>
2017-02-12 21:49:33 +0300

    new styles

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<link href="css/style.css" rel="stylesheet" 
 		<title>Document</title>
 	</head>
 	<body>
 	<h1>GIT</h1>
 	<p>New change</p>
+	<p>New change1</p>
+	<p>New change2</p>
 	</body>
 </html>
